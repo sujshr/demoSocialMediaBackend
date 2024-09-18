cdf1f2e4ee667e802d42cb6ac0cf4f3fc8fe3d0c
sujshr <sujshrest@gmail.com>
2024-09-18 12:22:58 +0530

    Prototype completed

--- src/socket/socketConfig.js
+++ src/socket/socketConfig.js
@@ -3,9 +3,9 @@ const socketIO = require("socket.io");
 const initializeSocketIO = (server) => {
   const io = socketIO(server, {
     cors: {
-      origin: true, // Allows any origin but still checks credentials
-      methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
-      credentials: true, // Allows sending cookies and other credentials
+      origin: "*",
+      methods: ["*"],
+      credentials: true,
     },
   });
 
